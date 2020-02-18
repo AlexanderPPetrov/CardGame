e82bd5fe486a273abae0afd5b0b794b3a53a7261
Aleksander Petrov <apetrovdev@gmail.com>
2020-02-18 07:39:29 +0200

    game styles updated

--- src/components/common/Card.vue
+++ src/components/common/Card.vue
@@ -1,174 +1,177 @@
 <template>
-  <div v-on:click="flipCard($event)" class="card-container js-card col-4 col-sm-3 my-1"
-       :class="{ switched: !card.isOpened }">
-    <div class="card-wrapper">
-      <div class="card-side d-flex align-items-center justify-content-center"
-           :class="{ active: card.isOpened, matched: card.isMatched, heartbeat: card.isMatched }">
-        <img :src="getCardFront" class="img-fluid vh-image"/>
-        <img :src="getDisabledOverlay" class="card-image vh-image img-fluid card-disabled-overlay"/>
-        <img :src="getImage" class="card-image img-fluid vh-image card-asset"/>
-        <span class="card-title">{{card.title}}</span>
-        <img :src="getStar" v-if="card.isMatched" class="star-image rotateScaleUp" :class="{puffOutCenter: card.starOut}"/>
-      </div>
-      <div class="card-side card-side-back d-flex align-items-center justify-content-center" :class="{ active: !card.isOpened }">
-        <img :src="getCardBack" class="img-fluid vh-image"/>
-      </div>
+    <div v-on:click="flipCard($event)" class="card-container js-card my-1 mx-1"
+         :class="{ switched: !card.isOpened }">
+        <div class="card-wrapper">
+            <div class="card-side d-flex align-items-center justify-content-center"
+                 :class="{ active: card.isOpened, matched: card.isMatched, heartbeat: card.isMatched }">
+                <img :src="getCardFront" class="vh-image"/>
+                <img :src="getDisabledOverlay" class="card-image vh-image card-disabled-overlay"/>
+                <img :src="getImage" class="card-image vh-image card-asset"/>
+                <span class="card-title" v-if="displayCardTitle">{{card.title}}</span>
+                <img :src="getStar" v-if="card.isMatched" class="star-image rotateScaleUp"
+                     :class="{puffOutCenter: card.starOut}"/>
+            </div>
+            <div class="card-side card-side-back d-flex align-items-center justify-content-center"
+                 :class="{ active: !card.isOpened }">
+                <img :src="getCardBack" class="vh-image"/>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  /* eslint-disable */
+    /* eslint-disable */
 
-  import mutations from '../../store/mutation-types';
-  import getters from '../../store/getters';
+    import mutations from '../../store/mutation-types';
+    import getters from '../../store/getters';
 
-  const cardTransitionTime = 500;
-  const openCardSound = '/static/media/select.mp3';
-  const closeCardSound = '/static/media/move.mp3';
-  const notMatchingCardSound = '/static/media/notmatching.mp3';
-  const matchCardSound = '/static/media/matching.mp3';
-  const levelSound = '/static/media/level.mp3';
+    const cardTransitionTime = 500;
+    const openCardSound = '/static/media/select.mp3';
+    const closeCardSound = '/static/media/move.mp3';
+    const notMatchingCardSound = '/static/media/notmatching.mp3';
+    const matchCardSound = '/static/media/matching.mp3';
+    const levelSound = '/static/media/level.mp3';
 
-  export default {
-    name: 'mt-card',
-    props: {
-      card: Object,
-    },
-    computed: {
-      getCardFront() {
-        const image = '/static/img/card_front.png';
-        return image;
-      },
-      getImage() {
-        const image = '/static/img/' + this.card.image;
-        return image;
-      },
-      getCardBack() {
-        const image = '/static/img/card_back.png';
-        return image;
-      },
-      getDisabledOverlay(){
-        const image = '/static/img/card_disabled_overlay.png';
-        return image;
-      },
-      getStar(){
-        const image = '/static/img/star.png';
-        return image;
-      },
-    },
-    data() {
-      return {
-        switching: false
-      };
-    },
-    methods: {
+    export default {
+        name: 'mt-card',
+        props: {
+            card: Object,
+            displayCardTitle: Boolean
+        },
+        computed: {
+            getCardFront() {
+                const image = '/static/img/card_front.png';
+                return image;
+            },
+            getImage() {
+                const image = '/static/img/' + this.card.image;
+                return image;
+            },
+            getCardBack() {
+                const image = '/static/img/card_back.png';
+                return image;
+            },
+            getDisabledOverlay() {
+                const image = '/static/img/card_disabled_overlay.png';
+                return image;
+            },
+            getStar() {
+                const image = '/static/img/star.png';
+                return image;
+            },
+        },
+        data() {
+            return {
+                switching: false
+            };
+        },
+        methods: {
 
-      closeOpenedCards(){
-        setTimeout(() => {
+            closeOpenedCards() {
+                setTimeout(() => {
 
-          const openedCards = getters.getOpenedCards(this.$store.state);
-          for (let i = 0; i < openedCards.length; i++) {
-            this.toggleOpenedCard(openedCards[i])
-          }
+                    const openedCards = getters.getOpenedCards(this.$store.state);
+                    for (let i = 0; i < openedCards.length; i++) {
+                        this.toggleOpenedCard(openedCards[i])
+                    }
 //          this.playSound(notMatchingCardSound);
-        }, cardTransitionTime * 2);
+                }, cardTransitionTime * 2);
 
-      },
+            },
 
-      toggleOpenedCard(card){
-        const newCard = Object.assign({}, card);
-        newCard.isOpened = !card.isOpened;
-        this.$store.commit(mutations.UPDATE_CARD, newCard);
-        if(newCard.isOpened){
-          this.playSound(openCardSound)
-        }else{
-          this.playSound(closeCardSound)
-        }
-      },
+            toggleOpenedCard(card) {
+                const newCard = Object.assign({}, card);
+                newCard.isOpened = !card.isOpened;
+                this.$store.commit(mutations.UPDATE_CARD, newCard);
+                if (newCard.isOpened) {
+                    this.playSound(openCardSound)
+                } else {
+                    this.playSound(closeCardSound)
+                }
+            },
 
-      canOpenMore(){
-        const openedCards = getters.getOpenedCards(this.$store.state);
-        if (openedCards.length == 2) {
-          return false;
-        }
-        return true;
-      },
-      setMatchedCards(){
-        const openedCards = getters.getOpenedCards(this.$store.state);
-        for (let i = 0; i < openedCards.length; i++) {
-          this.markCardAsMatched(openedCards[i]);
-        }
-        setTimeout(() => {
-          this.playSound(matchCardSound);
-        }, cardTransitionTime / 2);
+            canOpenMore() {
+                const openedCards = getters.getOpenedCards(this.$store.state);
+                if (openedCards.length == 2) {
+                    return false;
+                }
+                return true;
+            },
+            setMatchedCards() {
+                const openedCards = getters.getOpenedCards(this.$store.state);
+                for (let i = 0; i < openedCards.length; i++) {
+                    this.markCardAsMatched(openedCards[i]);
+                }
+                setTimeout(() => {
+                    this.playSound(matchCardSound);
+                }, cardTransitionTime / 2);
 
-        this.checkGameFinished();
-      },
+                this.checkGameFinished();
+            },
 
-      checkGameFinished(){
-        const matchedCards = getters.getMatchedCards(this.$store.state);
-        if(matchedCards.length == this.$store.state.cards.length){
-          setTimeout(() => {
-            this.playSound(levelSound);
-            this.$store.commit(mutations.SET_SHOW_CONGRATS, true);
-          }, 2000);
-          setTimeout(() => {
-            this.$store.commit(mutations.SET_GAME_FINISHED, true);
-            this.$store.commit(mutations.SET_SHOW_CONGRATS, false);
-            this.$store.commit(mutations.SET_PLAY_TIME, (new Date().getTime() - this.$store.state.startTime)/1000);
-          }, 6000);
+            checkGameFinished() {
+                const matchedCards = getters.getMatchedCards(this.$store.state);
+                if (matchedCards.length == this.$store.state.cards.length) {
+                    setTimeout(() => {
+                        this.playSound(levelSound);
+                        this.$store.commit(mutations.SET_SHOW_CONGRATS, true);
+                    }, 2000);
+                    setTimeout(() => {
+                        this.$store.commit(mutations.SET_GAME_FINISHED, true);
+                        this.$store.commit(mutations.SET_SHOW_CONGRATS, false);
+                        this.$store.commit(mutations.SET_PLAY_TIME, (new Date().getTime() - this.$store.state.startTime) / 1000);
+                    }, 6000);
 
-        }
-      },
+                }
+            },
 
-      playSound (sound) {
-        if(sound) {
-          var audio = new Audio(sound);
-          audio.play();
-        }
-      },
-      markCardAsMatched(card){
-        const newCard = Object.assign({}, card);
-        newCard.isMatched = true;
-        this.$store.commit(mutations.UPDATE_CARD, newCard);
-        setTimeout(() => {
-          newCard.starOut = true;
-          this.$store.commit(mutations.UPDATE_CARD, newCard);
-        }, 2000);
-      },
+            playSound(sound) {
+                if (sound) {
+                    var audio = new Audio(sound);
+                    audio.play();
+                }
+            },
+            markCardAsMatched(card) {
+                const newCard = Object.assign({}, card);
+                newCard.isMatched = true;
+                this.$store.commit(mutations.UPDATE_CARD, newCard);
+                setTimeout(() => {
+                    newCard.starOut = true;
+                    this.$store.commit(mutations.UPDATE_CARD, newCard);
+                }, 2000);
+            },
 
-      flipCard(){
-        if (this.switching) {
-          return false
-        }
+            flipCard() {
+                if (this.switching) {
+                    return false
+                }
 
-        const canOpenMore = this.canOpenMore();
+                const canOpenMore = this.canOpenMore();
 
-        if (!canOpenMore || this.card.isMatched) {
-          return;
-        }
+                if (!canOpenMore || this.card.isMatched) {
+                    return;
+                }
 
-        this.switching = true;
-        setTimeout(() => {
+                this.switching = true;
+                setTimeout(() => {
 
-          this.toggleOpenedCard(this.card);
-          const openedCards = getters.getOpenedCards(this.$store.state);
-          if (openedCards.length == 2) {
-            if (openedCards[0].type == openedCards[1].type) {
-              // Cards matching
-              this.setMatchedCards();
-            } else {
-              // Cards not matching
-              this.closeOpenedCards();
-            }
-            this.$store.commit(mutations.SET_COUNT, this.$store.state.tryCount + 1);
-          }
+                    this.toggleOpenedCard(this.card);
+                    const openedCards = getters.getOpenedCards(this.$store.state);
+                    if (openedCards.length == 2) {
+                        if (openedCards[0].type == openedCards[1].type) {
+                            // Cards matching
+                            this.setMatchedCards();
+                        } else {
+                            // Cards not matching
+                            this.closeOpenedCards();
+                        }
+                        this.$store.commit(mutations.SET_COUNT, this.$store.state.tryCount + 1);
+                    }
 
-          this.switching = false
-        }, cardTransitionTime / 2);
+                    this.switching = false
+                }, cardTransitionTime / 2);
 
-      },
-    },
-  };
+            },
+        },
+    };
 </script>
--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -1,154 +1,177 @@
 <template>
-  <div class="h-100 game-content d-flex align-items-center" :class="{ fadeIn: isLoaded }">
-    <div class="container align-items-center justify-content-center d-flex main-container">
-      <div class="row">
-        <mt-card v-if="cardResults.length"
-                 v-for="(card) in cardResults"
-                 :key="card.id"
-                 :card="card"
-        ></mt-card>
-      </div>
-      <b-modal id="modalGameEnd"
-               ref="modal"
-               @ok="newGame"
-               v-model="gameFinished"
-               no-close-on-esc
-               no-close-on-backdrop
-               centered
-               cancel=false
-               size="lg"
-               ok-variant="info btn-lg"
-               ok-only
-               hide-header-close
-      >
-        <div class="h-100 d-flex justify-content-center flex-column">
-          <div class="result-header">Твоят резултат</div>
-          <div class="d-flex w-100 my-5">
-            <div class="stats-container align-items-center justify-content-center text-center d-flex flex-column">
-              <div class="d-flex align-items-center">
-                <i class="fas fa-star icon-star mr-3"></i>
-                <span class="result">
+    <div class="h-100 game-content d-flex align-items-center" :class="{ fadeIn: isLoaded }">
+        <div class="align-items-center justify-content-center d-flex main-container flex-column">
+            <div class="row align-items-center justify-content-center">
+                <mt-card v-if="cardResults.length"
+                         v-for="(card) in cardResultsFirst"
+                         :key="card.id"
+                         :card="card"
+                         :displayCardTitle="displayCardTitle"
+                ></mt-card>
+            </div>
+            <div class="row align-items-center justify-content-center">
+                <mt-card v-if="cardResults.length"
+                         v-for="(card) in cardResultsSecond"
+                         :key="card.id"
+                         :card="card"
+                         :displayCardTitle="displayCardTitle"
+                ></mt-card>
+            </div>
+            <b-modal id="modalGameEnd"
+                     ref="modal"
+                     @ok="newGame"
+                     v-model="gameFinished"
+                     no-close-on-esc
+                     no-close-on-backdrop
+                     centered
+                     cancel=false
+                     size="lg"
+                     ok-variant="info btn-lg"
+                     ok-only
+                     hide-header-close
+            >
+                <div class="h-100 d-flex justify-content-center flex-column">
+                    <div class="result-header">Твоят резултат</div>
+                    <div class="d-flex w-100 my-5">
+                        <div
+                            class="stats-container align-items-center justify-content-center text-center d-flex flex-column">
+                            <div class="d-flex align-items-center">
+                                <i class="fas fa-star icon-star result-icon mr-3"></i>
+                                <span class="result">
                 {{getTryCount}}
               </span>
-              </div>
-              <div class="result-label">Опити</div>
-            </div>
-            <div class="stats-container align-items-center justify-content-center text-center d-flex flex-column">
-              <div class="d-flex align-items-center">
-                <i class="fas fa-stopwatch icon-clock mr-3"></i>
-                <span class="result">
+                            </div>
+                            <div class="result-label">Опити</div>
+                        </div>
+                        <div
+                            class="stats-container align-items-center justify-content-center text-center d-flex flex-column">
+                            <div class="d-flex align-items-center">
+                                <i class="fas fa-stopwatch icon-clock result-icon mr-3"></i>
+                                <span class="result">
                 {{ getPlayTime | moment("utc") |  moment("HH:mm:ss") }}
               </span>
-              </div>
-              <div class="result-label">Време</div>
-            </div>
-            <div class="stats-container align-items-center justify-content-center text-center d-flex flex-column">
-              <div class="d-flex align-items-center">
-                <i class="fas fa-gamepad icon-level mr-3"></i>
-                <span class="result">
+                            </div>
+                            <div class="result-label">Време</div>
+                        </div>
+                        <div
+                            class="stats-container align-items-center justify-content-center text-center d-flex flex-column">
+                            <div class="d-flex align-items-center">
+                                <i class="fas fa-gamepad icon-level result-icon mr-3"></i>
+                                <span class="result">
                 {{ getCurrentLevel }}
               </span>
-              </div>
-              <div class="result-label">Трудност</div>
+                            </div>
+                            <div class="result-label">Трудност</div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <b-button v-on:click="newGame" variant="info" size="lg" class="d-flex align-items-center">
+                            <i class="fa fa-play-circle btn-icon  mr-3"></i>
+                            <span>НОВА ИГРА</span>
+                        </b-button>
+                    </div>
+                </div>
+                <div slot="modal-footer"></div>
+            </b-modal>
+        </div>
+        <div v-if="showCongrats">
+<!--            <div class="pyro pyro-1">-->
+<!--                <div class="before"></div>-->
+<!--                <div class="after"></div>-->
+<!--            </div>-->
+<!--            <div class="pyro pyro-2">-->
+<!--                <div class="before"></div>-->
+<!--                <div class="after"></div>-->
+<!--            </div>-->
+            <div class="congrats-container d-flex justify-content-center align-items-center flex-column">
+                <img :src="getBravo" class="bravo-image heartbeat"/>
             </div>
-          </div>
-          <div class="d-flex justify-content-center">
-            <b-button v-on:click="newGame" variant="info" size="lg" class="d-flex align-items-center">
-              <i class="fa fa-play-circle btn-icon mr-3"></i>
-              <span>НОВА ИГРА</span>
-            </b-button>
-          </div>
         </div>
-        <div slot="modal-footer"></div>
-      </b-modal>
-    </div>
-      <div v-if="showCongrats">
-        <div class="pyro pyro-1">
-          <div class="before"></div>
-          <div class="after"></div>
-        </div>
-        <div class="pyro pyro-2">
-          <div class="before"></div>
-          <div class="after"></div>
-        </div>
-        <div class="congrats-container d-flex justify-content-center align-items-center flex-column">
-          <img :src="getBravo" class="bravo-image heartbeat"/>
-        </div>
-      </div>
 
-  </div>
+    </div>
 </template>
 
 <script>
-  import MtCard from '../common/Card';
-  import mutations from '../../store/mutation-types';
+    import MtCard from '../common/Card';
+    import mutations from '../../store/mutation-types';
 
-  export default {
-    data(){
-      return {
-        isLoaded: false
-      }
-    },
-    created(){
+    export default {
+        data() {
+            return {
+                isLoaded: false,
+                displayCardTitle: false
+            }
+        },
+        created() {
 
-      setTimeout(() => {
-        this.isLoaded = true;
-        this.startGame();
-      }, 1000);
-    },
-    computed: {
-      cardResults() {
-        return this.$store.state.cards;
-      },
-      gameFinished(){
-        return this.$store.state.gameFinished;
-      },
-      getTryCount(){
-        return this.$store.state.tryCount;
-      },
-      getPlayTime(){
-        return this.$store.state.playTime;
-      },
-      getCurrentLevel(){
-        return this.$store.state.currentLevel + 1;
-      },
-      getBravo(){
-        const image = '/static/img/bravo.png';
-        return image;
-      },
-      showCongrats(){
-        return this.$store.state.showCongrats;
-      }
-    },
+            setTimeout(() => {
+                this.isLoaded = true;
+                this.startGame();
+            }, 1000);
+        },
+        computed: {
+            cardResults() {
+                return this.$store.state.cards;
+            },
+            cardResultsFirst() {
+                return this.$store.state.cards.slice(0, this.$store.state.cards.length / 2);
+            },
+            cardResultsSecond() {
+                return this.$store.state.cards.slice(this.$store.state.cards.length / 2, this.$store.state.cards.length);
+            },
+            gameFinished() {
+                return this.$store.state.gameFinished;
+            },
+            getTryCount() {
+                return this.$store.state.tryCount;
+            },
+            getPlayTime() {
+                return this.$store.state.playTime;
+            },
+            getCurrentLevel() {
+                return this.$store.state.currentLevel + 1;
+            },
+            getBravo() {
+                const image = '/static/img/bravo.png';
+                return image;
+            },
+            showCongrats() {
+                return this.$store.state.showCongrats;
+            }
+        },
 
-    methods: {
-      startGame(){
-        this.$store.commit(mutations.SET_START_TIME, new Date().getTime());
-        this.$store.commit(mutations.SET_COUNT, 0);
-        const currentLevel = this.$store.state.currentLevel;
-        const cards = this.$store.state.cardsData[currentLevel].level;
-        this.$store.commit(mutations.SET_CARDS, cards);
-      },
-      newGame(){
-        this.$store.commit(mutations.SET_GAME_FINISHED, false);
-        setTimeout(() => {
-          let nextLevel = this.$store.state.currentLevel + 1;
-          if(nextLevel >= this.$store.state.cardsData.length -1){
-              nextLevel = this.$store.state.cardsData.length -1;
-          }
-          this.$store.commit(mutations.SET_CURRENT_LEVEL, nextLevel);
-          this.startGame();
-        }, 250)
+        methods: {
+            startGame() {
+                this.displayCardTitle = false;
+                this.$store.commit(mutations.SET_START_TIME, new Date().getTime());
+                this.$store.commit(mutations.SET_COUNT, 0);
+                const currentLevel = this.$store.state.currentLevel;
+                const cards = this.$store.state.cardsData[currentLevel].level;
+                this.$store.commit(mutations.SET_CARDS, cards);
+                setTimeout(()=>{
+                    this.displayCardTitle = true;
+                }, 1000)
+            },
+            newGame() {
+                this.$store.commit(mutations.SET_GAME_FINISHED, false);
+                setTimeout(() => {
+                    let nextLevel = this.$store.state.currentLevel + 1;
+                    if (nextLevel >= this.$store.state.cardsData.length - 1) {
+                        nextLevel = this.$store.state.cardsData.length - 1;
+                    }
+                    this.$store.commit(mutations.SET_CURRENT_LEVEL, nextLevel);
+                    this.startGame();
+                }, 250)
 
-      }
-    },
-    components: {
-      MtCard,
-    },
-  };
+            }
+        },
+        components: {
+            MtCard,
+        },
+    };
 </script>
 <style lang="scss">
-  label {
-    color: #7e7e7e;
-  }
+    label {
+        color: #7e7e7e;
+    }
 </style>
